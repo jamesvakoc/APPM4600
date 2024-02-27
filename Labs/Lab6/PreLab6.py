524f47ab2c17ddb9bb781fcbac66ca70d5ec619e
James Vakoc <jamesvakoc@Jamess-MacBook-Air-2.local>
2024-02-27 10:52:48 -0700

    Added Slacker Newton Code for Lab 6

--- Labs/Lab6/PreLab6.py
+++ Labs/Lab6/PreLab6.py
@@ -7,24 +7,25 @@ def driver():
 
     #define vector of h values, halfing the value at each itteration
     h=0.01*2.**(-np.arange(0,10))
+    print(h)
 
     #define value at which we're approximating the derivative
     s=np.pi/2
 
     #calcualting forward and centered differences
-    fd = (np.cos(s+h)-np.cos(s))/h
-    cd = (np.cos(s+h)-np.cos(s-h))/(2*h)
+    fderror = ((np.cos(s+h)-np.cos(s))/h)
+    cd = ((np.cos(s+h)-np.cos(s-h))/(2*h))
 
     #calculating associated errors
-    fderror = np.absolute(fd+1)
-    cderror = np.absolute(cd+1)
+    fderror = np.absolute(((np.cos(s+h)-np.cos(s))/h)+1)
+    cderror = np.absolute(((np.cos(s+h)-np.cos(s-h))/(2*h))+1)
     
-    print('The forward difference approximations for the derivative of f at s are', fd)
-    print('The centered difference approximations for the derivative of f at s are', cd)
+    print('The forward difference approximations for the derivative of f at s are', ((np.cos(s+h)-np.cos(s))/h))
+    print('The centered difference approximations for the derivative of f at s are', ((np.cos(s+h)-np.cos(s-h))/(2*h)))
     
     #plotting errors as a function of h
-    plt.plot(fderror,h,'r',label='Forward Difference Error')
-    plt.plot(cderror,h,'b',label='Centered Difference Error')
+    plt.plot(h,fderror,'r',label='Forward Difference Error')
+    plt.plot(h,cderror,'b',label='Centered Difference Error')
     plt.xscale('log')
     plt.yscale('log')
     plt.xlabel('h')
