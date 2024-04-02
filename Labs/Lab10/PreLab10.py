8a0183743d5a26e42a2836880fc45fec690b7576
James Vakoc <jamesvakoc@Jamess-MacBook-Air-2.local>
2024-04-01 23:38:45 -0600

    Added Comments

--- Labs/Lab10/PreLab10.py
+++ Labs/Lab10/PreLab10.py
@@ -1,15 +1,19 @@
+#import libraries
 import numpy as np
 
 def driver():
 
+#define parameters
     n=5
     x=7
 
+#call function
     p = eval_legendre(n,x)
     print(p)
 
     return
 
+#define and evaluate legendre polynomials
 def eval_legendre(n,x):
     p=np.zeros(n+1)
 
